6335e02c08e72ae64f4253dde31fe32b02aae5ef
venkatmiriyala19 <venkatmiriyala19@gmail.com>
2024-12-06 16:56:51 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import '../styles/navbar.css';
-import tenantImage from '../images/tenant.jpg';
-import { FaSearch } from 'react-icons/fa';
+import React from "react";
+import "../styles/navbar.css";
+import tenantImage from "../images/tenant.jpg";
+import { FaSearch } from "react-icons/fa";
 const Navbar = () => {
-  const user = JSON.parse(localStorage.getItem('user'))
+  const user = JSON.parse(localStorage.getItem("user"));
   return (
     <nav className="navbar">
       <div className="navbar-left">
@@ -12,10 +12,19 @@ const Navbar = () => {
       <div className=" flex gap-10">
         <div style={styles.searchBar}>
           <FaSearch style={styles.searchIcon} />
-          <input type="text" placeholder="Search..." style={styles.searchInput} />
+          <input
+            type="text"
+            placeholder="Search..."
+            style={styles.searchInput}
+          />
+          <button style={styles.searchButton}>Search</button>
         </div>
         <div className="user-profile">
-          <img src={tenantImage} alt="User" className='w-28 h-24 rounded-full object-cover' />
+          <img
+            src={tenantImage}
+            alt="User"
+            className="w-28 h-24 rounded-full object-cover"
+          />
           <span className="username">{user?.name}</span>
         </div>
       </div>
@@ -24,26 +33,31 @@ const Navbar = () => {
 };
 const styles = {
   searchBar: {
-    display: 'flex',
-    alignItems: 'center',
-    borderRadius: '25px',
-    backgroundColor: '#fff',
-    padding: '5px 10px',
-    boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)',
-    width: '330px',
-    height: '40px',
+    display: "flex",
+    alignItems: "center",
+    borderRadius: "25px",
+    backgroundColor: "#fff",
+    padding: "5px 15px",
+    boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
+    width: "350px",
+    height: "45px",
+    marginRight: "1rem",
+    fontFamily: "Poppins",
   },
   searchIcon: {
-    color: '#555',
-    marginRight: '5px',
+    color: "#555",
+    marginRight: "10px",
+    fontSize: "1.5rem",
   },
   searchInput: {
-    border: 'none',
-    outline: 'none',
-    width: '100%',
-    fontSize: '16px',
+    border: "none",
+    outline: "none",
+    width: "100%",
+    fontSize: "16px",
+    color: "#505254",
   },
-}
+  searchButton: {
+    color: "#505254",
+  },
+};
 export default Navbar;
-
-
