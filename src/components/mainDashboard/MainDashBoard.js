200a273209940e566127544ceb268a013b7d6a54
GVK <venkatakousikcse01@gmail.com>
2024-12-23 10:13:15 +0530

    feat:minimal changes in dashboard

--- src/components/mainDashboard/MainDashBoard.js
+++ src/components/mainDashboard/MainDashBoard.js
@@ -41,21 +41,19 @@ const MainDashboard = () => {
               <div>
                 <div className="flex gap-4">
                   <button
-                    className={`py-2 px-6 font-semibold transition-colors duration-300 rounded-lg ${
-                      activeSection === "visit"
-                        ? "bg-[#6F84F8] text-white shadow-lg" // Updated color
-                        : "bg-gray-200 text-black hover:bg-gray-300"
-                    }`}
+                    className={`py-2 px-6 font-semibold transition-colors duration-300 rounded-lg ${activeSection === "visit"
+                      ? "bg-[#6F84F8] text-white shadow-lg" // Updated color
+                      : "bg-gray-200 text-black hover:bg-gray-300"
+                      }`}
                     onClick={() => setActiveSection("visit")}
                   >
                     Visit Compliance
                   </button>
                   <button
-                    className={`py-2 px-6 font-semibold transition-colors duration-300 rounded-lg ${
-                      activeSection === "reassessment"
-                        ? "bg-[#6F84F8] text-white shadow-lg" // Updated color
-                        : "bg-gray-200 text-black hover:bg-gray-300"
-                    }`}
+                    className={`py-2 px-6 font-semibold transition-colors duration-300 rounded-lg ${activeSection === "reassessment"
+                      ? "bg-[#6F84F8] text-white shadow-lg" // Updated color
+                      : "bg-gray-200 text-black hover:bg-gray-300"
+                      }`}
                     onClick={() => setActiveSection("reassessment")}
                   >
                     Tenant Reassessment
@@ -90,23 +88,22 @@ const MainDashboard = () => {
               <PayPeriodPieChart style={{ height: "150px" }} />
             </div>
             <div className="flex flex-col flex-2">
+              <h3 className="mt-2 mb-2">Info</h3>
               <div className="flex gap-2">
                 <button
-                  className={`py-1 px-2 rounded-lg ${
-                    activeView === "tenant"
-                      ? "bg-[#6F84F8] text-white shadow-lg"
-                      : "bg-gray-200"
-                  }`}
+                  className={`py-1 px-2 rounded-lg ${activeView === "tenant"
+                    ? "bg-[#6F84F8] text-white shadow-lg"
+                    : "bg-gray-200"
+                    }`}
                   onClick={() => setActiveView("tenant")}
                 >
                   Tenant
                 </button>
                 <button
-                  className={`py-1 px-2 rounded-lg ${
-                    activeView === "hcm"
-                      ? "bg-[#6F84F8] text-white shadow-lg"
-                      : "bg-gray-200"
-                  }`}
+                  className={`py-1 px-2 rounded-lg ${activeView === "hcm"
+                    ? "bg-[#6F84F8] text-white shadow-lg"
+                    : "bg-gray-200"
+                    }`}
                   onClick={() => setActiveView("hcm")}
                 >
                   HCM
@@ -115,16 +112,13 @@ const MainDashboard = () => {
               <div className="mt-2">
                 {activeView === "tenant" && (
                   <>
-                    <h3 className="mt-2 ml-2">Tenant</h3>
                     <TenantInfoChart style={{ height: "150px" }} />{" "}
-                    {/* Adjusted height */}
                   </>
                 )}
                 {activeView === "hcm" && (
                   <>
                     <h3 className="mt-2 ml-2">HCM</h3>
                     <HCMInfoChart style={{ height: "150px" }} />{" "}
-                    {/* Adjusted height */}
                   </>
                 )}
               </div>
