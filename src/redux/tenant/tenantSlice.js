115982cb64824e0cebaa9777e58e287921edd8a4
Surya-0804 <surya.abothula@gmail.com>
2025-01-09 08:30:48 +0530

    Fixing the tennat Documents slice and uploading

--- src/redux/tenant/tenantSlice.js
+++ src/redux/tenant/tenantSlice.js
@@ -63,6 +63,11 @@ const initialState = {
     names: [],
   },
   services: [],
+  files: {
+    "Intake Documents": [],
+    "ID Proofs": [],
+    "Lease Agreements": [],
+  },
 };
 
 const tenantSlice = createSlice({
@@ -98,6 +103,33 @@ const tenantSlice = createSlice({
     clearServices: (state) => {
       state.services = []; // Clear all services from Redux store
     },
+    // **File Management Actions**
+    addFileToFolder: (state, action) => {
+      const { folderName, file } = action.payload;
+      if (!state.files[folderName]) {
+        state.files[folderName] = []; // Create folder if it doesn't exist
+      }
+      state.files[folderName].push(file); // Add file to the folder
+    },
+    removeFileFromFolder: (state, action) => {
+      const { folderName, fileName } = action.payload;
+      if (state.files[folderName]) {
+        state.files[folderName] = state.files[folderName].filter(
+          (file) => file.name !== fileName
+        );
+      }
+    },
+    createFolder: (state, action) => {
+      const { folderName } = action.payload;
+      if (!state.files[folderName]) {
+        state.files[folderName] = []; // Add a new folder
+      }
+    },
+    // Add to your reducers in hcmSlice
+    removeFolder: (state, action) => {
+      const { folderName } = action.payload;
+      delete state.files[folderName];
+    },
   },
 });
 
@@ -110,6 +142,11 @@ export const {
   updateService,
   removeService,
   clearServices,
+  resetHcmInfo, // Exporting the reset action
+  addFileToFolder, // Add file to folder
+  removeFileFromFolder, // Remove file from folder
+  createFolder, // Create new folder
+  renameFolder,
 } = tenantSlice.actions;
 
 export default tenantSlice.reducer;
